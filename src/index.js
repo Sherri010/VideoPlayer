77653775ede87486b3d12663547f10067fadf313
sherri010 <aminshahidi.sh@gmail.com>
2017-02-25 15:55:54 -0800

    adds file structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
+//importing React
+import React from 'react'
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//youtube api key
+const API_KEY = 'AIzaSyCUKSl78-hR09poPKX0qPVtX6eS1fdVYAk';
+//creade the component
+const App = () => {
+  return <div> Sherri </div>;
+}
+//append it to the DOM
+ReactDOM.render(<App />, document.getElementById("container"));
