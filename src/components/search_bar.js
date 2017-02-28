e264db1296420609a32f4859c9e90c550695f2f6
sherri010 <aminshahidi.sh@gmail.com>
2017-02-28 07:06:17 -0800

    adds state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,30 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    //when ever the user enters something new in the field,
+    //this will be updated
+    this.state = { term:' '};
+  }
+
   render(){
+
     //must return jsx
-    return <input />;
+    return (
+      <div>
+        <input onChange = { event => this.setState({term: event.target.value})} />;
+        value of input: {this.state.term}
+    </div>
+    );
+    //return <input onChange={this.onInputChange} />;
+    // <input onChange={ (event) => console.log(event.target)} />;
+    // <input onChange={ event => console.log(event.target)} />;
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
   }
 }
 
